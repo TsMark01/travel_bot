21efd5789d6607f7964c4a430ca51910632d07f9
Mark Tsyrul <151933176+TsMark01@users.noreply.github.com>
2025-09-27 20:07:59 +0600

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,3 @@ MAX_USER_GPT_TOKENS = 4000
 TOKENIZE_URL = "https://llm.api.cloud.yandex.net/foundationModels/v1/tokenizeCompletion"
 GPT_MODEL = 'yandexgpt-lite'
 GPT_URL = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
-
-
